b3b77282d7f7d636c8e610d2a4be624c015dd182
zulkufseckin <zulkuf.seckin@mobven.com>
2022-01-14 16:15:46 +0300

    initial

---
+++ todo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  todo
+//
+//  Created by Z.Seckin on 14.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
